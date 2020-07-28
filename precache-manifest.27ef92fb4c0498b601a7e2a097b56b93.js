5a82d07b1ba47f8e97502fc83fa6644776013847
felipe <feliperarnau@gmail.com>
2020-07-28 18:37:32 -0300

    Updates

---
+++ precache-manifest.27ef92fb4c0498b601a7e2a097b56b93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a18b39ef1ed75cd8c1da3508ce9f7d9b",
+    "url": "/zoitawood/index.html"
+  },
+  {
+    "revision": "75819ca74996909db8b2",
+    "url": "/zoitawood/static/css/main.2ff8e7c3.chunk.css"
+  },
+  {
+    "revision": "d4c86bd0228a7ce1fe97",
+    "url": "/zoitawood/static/js/2.e3eb26f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zoitawood/static/js/2.e3eb26f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75819ca74996909db8b2",
+    "url": "/zoitawood/static/js/main.6f48c7d3.chunk.js"
+  },
+  {
+    "revision": "6c108b96a3e5983a8250",
+    "url": "/zoitawood/static/js/runtime-main.df89059d.js"
+  }
+]);
